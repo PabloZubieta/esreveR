6f60efcb3878c08e8c7e2615abaa805f030d4c0f
KUNZLI Christophe <Christophe.KUNZLI@cpnv.ch>
2022-05-19 16:53:54 +0200

    Refactor Format

--- TextManager/TextFormater.cs
+++ TextManager/TextFormater.cs
@@ -8,32 +8,45 @@ namespace TextManager
         #region public methods
         public string Reverse(string textToReverse)
         {
-            textToReverse = textToReverse.Remove(textToReverse.Length - 1, 1);
-            textToReverse = textToReverse.ToLower();
-
-            String[] splitted = textToReverse.Split(" ");
+            string formatted = Format(textToReverse,true);
+            String[] splitted = formatted.Split(" ");
             String[] reversed = new string[splitted.Length];
 
-            for (int i = 0; i < splitted.Length; i++)
+            for (int i = splitted.Length - 1; i >= 0; i--)
             {
                 reversed[(reversed.Length - 1) - i] = splitted[i];
             }
             String res = String.Join(" ", reversed);
-            
-            char[] Caps = res.ToCharArray();
-            Caps[0]= char.ToUpper(Caps[0]);
 
-            res = new string(Caps);
-            res += ".";
+            res = Format(res, false);
 
             return res;
-            //throw new NotImplementedException();
         }
-
-
         #endregion public methods
 
         #region private methods
+        private string Format(string textToFormat,bool lower) {
+            string formated;
+            if (lower)
+            {
+                formated = textToFormat.Remove(textToFormat.Length - 1, 1);
+                formated = formated.ToLower();
+
+
+            }
+            else {
+                char[] caps = textToFormat.ToCharArray();
+                caps[0] = char.ToUpper(caps[0]);
+
+                formated = new string(caps);
+               formated += ".";
+
+            }
+            
+            return formated;
+        }
+
+
         #endregion private methods
     }
 }
